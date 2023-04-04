9c146ae3d7def7bb86f4a9e22d349f07ad0772a3
Your Name <quenemdi831@gmail.com>
2023-04-04 10:11:28 +0700

    fix format money

--- src/main/resources/static/admin/js/custom.js
+++ src/main/resources/static/admin/js/custom.js
@@ -234,7 +234,7 @@ $(document).ready(function () {
     responsiveDataTable.DataTable({
       "aLengthMenu": [[10, 20, 30, 50, 75, -1], [10, 20, 30, 50, 75, "All"]],
       "pageLength": 10,
-      "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">'
+      "dom": '<"row justify-content-between top-information"lf>rt<"row justify-content-between bottom-information"ip><"clear">',
     });
   }
 
